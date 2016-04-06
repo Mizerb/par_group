e6bc7e3b90504d43a05ae60de34b4b919d054570
toadstooliv <aaronpbrooks@outlook.com>
2016-04-06 22:48:23 +0000

    Adding things

---
+++ Calculation.h
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include <string.h>
+#include <mpi.h>
+#include <pthread.h>
+
+
+
+void run_threadpool(  )
--- main.c
+++ main.c
@@ -13,27 +13,68 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include<string.h>
+#include <string.h>
 #include <mpi.h>
+#include <pthread.h>
 
 #include "Generation.h"
 #include "Calculation.h"
 #include "Documentation.h"
 
-/* TODO: macro for taking x,y coords and fetching matrix location */
-
-/* TODO: macro for getting the current absolute position in matrix*/
-
-
-int main(int argc, const char* argc)
+typedef struct
 {
-    
     int mpi_rank;
     int mpi_commsize;
     int pthreads_per_mpi;
     int matrix_size;
     int matrix_slice_height;
     int* matrix_data;
+    pthread
+} program_info;
+
+
+/* TODO: macro for taking x,y coords and fetching matrix location */
+
+/* TODO: macro for getting the current absolute position in matrix*/
+
+program_info grab_args(int argc, char** argv)
+{
+    
+    if(argc != 3)
+    {
+        fprintf(stderr , "Usage: \n 1: Matrix Size (2^Z  for X by X matrix\n 2: Pthreads per mpi rank (more than 0)\n");
+        exit( EXIT_FAILURE);
+    }
+     
+    
+    program_info Ret;
+    
+    Ret.matrix_size = atoi(argv[1]);
+    
+    if( (Ret.matrix_size %2 != 0)  || Ret.matrix_size < 1 )
+    {
+        fprintf(stderr, "Matrix_Size Usage: 2^Z for the X by X matrix size\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    Ret.pthreads_per_mpi = atoi(argv[2]);
+    
+    if( Ret.pthreads_per_mpi < 0 )
+    {
+        fprintf(stderr, "Pthreads per mpi rank must be at least 0\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    // Other input fun;
+    
+    return Ret;
+    
+}
+
+
+int main(int argc,  char* argv[])
+{
+    program_info inf;
   
     /* get inputs from command line */
      
@@ -43,12 +84,8 @@ int main(int argc, const char* argc)
         3. Other?
     */
     
-    if(argc != 3)
-    {
-        fprintf(stderr , "Wrong number of inputs\n");
-        return EXIT_FAILURE;
-    }
-     
+    inf = grab_args(argc, argv);
+    
     /* set up MPI ranks and communication */
     
     MPI_Init( &argc, &argv);
@@ -58,12 +95,22 @@ int main(int argc, const char* argc)
     matrix_slice_height = matrix_size/mpi_commsize;
     
     MPI_Barrier( MPI_COMM_WORLD );
+    /* start timer here? */
+    
 
     /* set up pthread pool and allocate matrix */
-    matrix_data = Generate_Matrix(matrix_size, matrix_slice_height);
-    //Where is the matrix? It's returning to nothing atm. 
+    
+    
+    
+    
+    inf.matrix_data = Generate_Matrix(matrix_size, matrix_slice_height);
+    
+    
+    
     /* MPI send */
-     
+    
+    
+    
     /* create ghost transpose rows */
      
     /* MPI recv */
@@ -73,4 +120,8 @@ int main(int argc, const char* argc)
      
     /*  */
     Write_Out_Matrix();
+    
+    
+    
+    
 }
