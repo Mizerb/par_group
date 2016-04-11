3acc04a008d4e57cf7ba784e6177b742a1328a10
toadstooliv <mizerb@rpi.edu>
2016-04-11 19:27:33 +0000

    updated documentation.c and Makefile

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 all:
-	mpicc main.c -o test.exe -lpthread
+	mpicc calculation.c documentation.c generation.c ./Timer/timer.c main.c -o test.exe -lpthread
--- documentation.c
+++ documentation.c
@@ -1,23 +1,59 @@
-#include "Documentation.h"
+#include "documentation.h"
+#include <string.h>
 
-#define CALL_ALL(foo) foo(matrix_data, mpi_rank, matrix_size, matrix_slice_height) 
+#define CALL_ALL(foo, blocked, file_count) foo(matrix_data, mpi_rank, matrix_size, matrix_slice_height, blocked, file_count) 
 
+#define blocked 1
+#define compact 0
 
-void 1_File_Block(int * matrix_data, 
+void File_Write(int * matrix_data, 
 	int mpi_rank, 
 	int matrix_size , 
-	int matrix_slice_height)
+	int matrix_slice_height,
+	int is_blocked,
+	int file_count)
 {
 	MPI_File fh;
 	MPI_Status status;
 	int fusize, nints;
-
-	int offset = mpi_rank * matrix_slice_height; //something like that
-
-	char* file_name;
+	int offset;
+	int size = matrix_slice_height * matrix_size; //something like that
+	// File name fun
+	char file_name[80] = "output";
+	char num_str[20];
+	sprintf(num_str, "%d" , mpi_rank/file_count);
+	strcat(file_name, num_str);
+	strcat(file_name, ".log");
+	//End of Filename fun
+	
+	if(is_blocked == blocked)
+	{
+		int eight_meg = 8000000;
+		
+		int blocks = ( (sizeof(int)*size) + eight_meg - 1)/ eight_meg;
+		offset  = eight_meg * blocks; 
+	}
+	else
+	{
+		offset = mpi_rank * sizeof(int) * matrix_slice_height * matrix_size;
+	}
+	
+	
 	MPI_FILE_open(MPI_COMM_WORLD, file_name, MPI_MODE_WRONLY|MPI_MODE_CREATE , MPI_INFO_NULL, &fh);
-
-	MPI_File_write_at(fh,offset, matrix_data, matrix_size, MPI_INT, &status);
+	
+	
+	if( file_count == 1)
+	{
+		MPI_File_write_at_all(fh, offset, matrix_data, size , MPI_INT, &status );
+	}
+	else
+	{
+		MPI_File_write_at(fh,offset, matrix_data, size, MPI_INT, &status);
+	}
+	
+	
+	
+	
 
 	MPI_File_close(&fh);
 }
@@ -31,31 +67,33 @@ void Write_OUt_Matrix(int * matrix_data,
 	int mode )
 {
 	//shit shit shit
-
+	//Tired, pain.. fkkk
 	switch(mode) {
 
 		case 0: 
-			CALL_ALL(1_File_Block);
+			CALL_ALL(File_Write, blocked , 1);
 			break;
 		case 1:
-			CALL_ALL(1_File_Boundary);
+			CALL_ALL(File_Write, compact , 1);
 			break;
 		case 2:
-			CALL_ALL(4_File_Block);
+			CALL_ALL(File_Write, blocked , 4);
 			break;
 		case 3:
-			CALL_ALL(4_File_Boundary);
+			CALL_ALL(File_Write, compact , 4);
 			break;
 		case 4:
-			CALL_ALL(8_File_Block);
+			CALL_ALL(File_Write, blocked , 8);
 			break;
 		case 5:
-			CALL_ALL(8_File_Boundary);
+			CALL_ALL(File_Write, compact , 8);
+			break;
 		case 6:
-			CALL_ALL(32_File_Block);
+			CALL_ALL(File_Write, blocked , 32);
+			break;
 		case 7:
-			CALL_ALL(32_File_Boundary);
-
+			CALL_ALL(File_Write, compact , 32);
+			break;
 	}
 
 }
